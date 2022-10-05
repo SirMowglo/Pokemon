3bda58341a17d690f2b4137cc84058d8c586e18d
Alejandro Damas <alejandrodamas3d@gmail.com>
2022-10-05 13:50:33 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialImportsModule } from './Modules/material-imports/material-imports.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
